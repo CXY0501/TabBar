288a42a1910fdade0414ea4d3e7118d54eaed032
CXY <chen.xy.2@foxmail.com>
2020-11-08 23:58:33 +0800

    project initiation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <tabbar>
+     <tabbaritem>
+       <img slot="itemicon" src="./assets/img/tabbar/首页 (1).png" alt="">
+       <img slot="itemicon_active" src="./assets/img/tabbar/首页.png" alt="">
+       <div slot="itemname">首页</div>
+     </tabbaritem>
+     <tabbaritem>
+       <img slot="itemicon" src="./assets/img/tabbar/类目 品类 分类 类别 (1).png" alt="">
+       <img slot="itemicon_active" src="./assets/img/tabbar/类目 品类 分类 类别.png" alt="">
+       <div slot="itemname">品类</div>
+     </tabbaritem>
+     <tabbaritem>
+       <img slot="itemicon" src="./assets/img/tabbar/购物车空 (1).png" alt="">
+       <img slot="itemicon_active" src="./assets/img/tabbar/购物车空.png" alt="">
+       <div slot="itemname">购物车</div>
+     </tabbaritem>
+     <tabbaritem>
+       <img slot="itemicon" src="./assets/img/tabbar/我的 (1).png" alt="">
+       <img slot="itemicon_active" src="./assets/img/tabbar/我的.png" alt="">
+       <div slot="itemname">我的</div>
+     </tabbaritem>
+   </tabbar>
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import tabbar from './components/tabbar/tabbar'
+import tabbaritem from './components/tabbar/tabbaritem'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   tabbar,
+   tabbaritem
   }
 }
 </script>
-
+  
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ @import "./assets/css/base.css";
 </style>
---
+++ src/components/tabbar/tabbaritem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="tab-bar-item">
+      <div v-if="!isActive" ><slot name="itemicon"></slot></div>
+      <div v-else><slot name="itemicon_active"></slot></div>
+      <div :class={active:isActive}><slot name="itemname"></slot></div>
+    </div>
+</template>
+
+<script>
+export default{
+  name:'tabbaritem',
+  data(){
+    return{
+      isActive:true
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+  .tab-bar-item{
+   flex: 1;
+   text-align: center;
+   height: 49px;
+   font-size: 14px;
+ }
+  .tab-bar-item img{
+    height: 24px;
+    width: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+  }
+  .active{
+    color:#d81e06;
+  }
+</style>
